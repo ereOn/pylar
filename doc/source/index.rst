d8daa29231a38cef4dc8dd91413f99240941e86f
Julien Kauffmann <julien.kauffmann@freelan.org>
2016-07-16 15:48:29 -0400

    Initial files

---
+++ doc/source/index.rst
@@ -0,0 +1,26 @@
+.. _index:
+
+Pylar - Documentation
+======================
+
+`Pylar` is an experiment on microservices.
+
+Installation
+------------
+
+You may install it by using `pip`::
+
+   pip install pylar
+
+Table of contents
+=================
+
+.. toctree::
+   :maxdepth: 2
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import (
+    setup,
+    find_packages,
+)
+
+setup(
+    name='pylar',
+    author='Julien Kauffmann',
+    author_email='julien.kauffmann@freelan.org',
+    maintainer='Julien Kauffmann',
+    maintainer_email='julien.kauffmann@freelan.org',
+    version=open('VERSION').read().strip(),
+    url='http://ereOn.github.io/pylar',
+    description=(
+        "An experiment on micro-services.",
+    ),
+    long_description="""\
+Pylar is an experiment on micro-services.
+""",
+    packages=find_packages(exclude=[
+        'tests',
+    ]),
+    install_requires=[
+        'azmq[csodium]>=1.0.0,<2',
+    ],
+    test_suite='tests',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Development Status :: 5 - Production/Stable',
+    ],
+)
