a3ee26a32fcc9a3daef2d61f12d21aa031c3b544
Julien Kauffmann <julien.kauffmann@freelan.org>
2016-08-04 23:33:36 -0400

    Added a more continuous client sample

--- client.py
+++ client.py
@@ -6,27 +6,31 @@ from pylar.client import Client
 from pylar.rpc_client_proxy import RPCClientProxy
 
 
+class MyClientProxy(RPCClientProxy):
+    @RPCClientProxy.notification_handler(use_context=True)
+    async def hello(self, context, *frames):
+        print("%s says hello !" % context)
+
+
 async def run(domain, endpoint):
     async with azmq.Context() as context:
         async with context.socket(azmq.DEALER) as socket:
             socket.connect(endpoint)
 
             async with Client(socket=socket) as client:
-                client_proxy = RPCClientProxy(
+                client_proxy = MyClientProxy(
                     client=client,
                     domain=domain,
                     credentials=b'password',
                 )
-                try:
-                    arithmetic_service = await client_proxy.get_rpc_service_proxy(
-                        b'service/arithmetic',
-                    )
-                    print(domain, await arithmetic_service.sum(2, 17))
-                except Exception as ex:
-                    print(domain, ex)
+                arithmetic_service = await client_proxy.get_rpc_service_proxy(
+                    b'service/arithmetic',
+                )
 
-                await client_proxy.notification(b'user/bob', 'hello')
-                await asyncio.sleep(1)
+                for x in range(60):
+                    print(domain, await arithmetic_service.sum(0, x))
+                    await client_proxy.notification(b'user/bob', 'hello')
+                    await asyncio.sleep(1)
 
 
 if __name__ == '__main__':
